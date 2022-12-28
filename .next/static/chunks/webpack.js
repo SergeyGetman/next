93a3eca036cfcdb8e35f8dd6b7ed0f0f49e136fa
Sergii Kushnerov <serhii.kushnerov.dev@gmail.com>
2022-12-29 00:19:34 +0200

    modified second part next

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "90f2aa4ec212a34f"; }
+/******/ 		__webpack_require__.h = function() { return "9f8890bf3e7698ef"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,18 +1,28 @@
 import {useRouter} from "next/router";
 import style from "../../style/user.module.scss"
 
-export default function User () {
-
+export default function User ({user}) {
+    console.log('this is user', user)
     const router = useRouter()
-    console.log('this is router', router)
     const {query} = router;
-    console.log('this is query', query)
     return (
         <div className={style.block}>
             <h1>
                 {`Пользователь number ${query.id}`}
             </h1>
+            <div>
+                {`Name User: ${user.name}`}
+            </div>
 
         </div>
     )
+}
+
+export async function getServerSideProps({params}) {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
+    const user = await response.json()
+
+    return {
+        props: {user}, // will be passed to the page component as props
+    }
 }
